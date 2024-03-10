2dd71452ca60af1991f71ead2e720071d02e36eb
Andrei Kostygov <kosandr17@mail.ru>
2024-03-10 23:20:00 +0300

    task 4

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,17 +1,32 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import lombok.SneakyThrows;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Testcontainers
 public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
     static {
-        POSTGRES = new PostgreSQLContainer<>("postgres:15")
+        POSTGRES = new PostgreSQLContainer<>("postgres:16")
             .withDatabaseName("scrapper")
             .withUsername("postgres")
             .withPassword("postgres");
@@ -20,8 +35,25 @@ public abstract class IntegrationTest {
         runMigrations(POSTGRES);
     }
 
+    @SneakyThrows({SQLException.class, LiquibaseException.class, FileNotFoundException.class})
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        Connection connection = DriverManager.getConnection(c.getJdbcUrl(), c.getUsername(), c.getPassword());
+
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+        String scrapperAbsolutePath = new File("").getAbsolutePath();
+        Path migrationsDirectoryPath = new File(scrapperAbsolutePath)
+            .toPath()
+            .getParent()
+            .resolve("migrations");
+
+        Liquibase
+            liquibase = new liquibase.Liquibase(
+                "master.xml",
+            new DirectoryResourceAccessor(migrationsDirectoryPath),
+            database);
+
+        liquibase.update(new Contexts(), new LabelExpression());
     }
 
     @DynamicPropertySource
