175241290b3c3fded84733fb09b7c32f0b764c51
Andrei Kostygov <kosandr17@mail.ru>
2024-03-17 12:34:12 +0300

    fix: small migration

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -9,12 +9,12 @@ create table Links(
     url TEXT not null,
     last_check_time TIMESTAMP with time zone not null DEFAULT CURRENT_TIMESTAMP,
     created_by TEXT not null ,
-    created_at TEXT not null,
+    created_at TIMESTAMP with time zone not null DEFAULT CURRENT_TIMESTAMP,
     last_modified_time TIMESTAMP with time zone not null
 );
 
 create table chats_links (
-    chat_id BIGINT not null,
-    link_id BIGINT not null,
+     chat_id BIGINT not null references Chats (id),
+     link_id BIGINT not null references Links (id),
     PRIMARY KEY(chat_id, link_id)
 );
