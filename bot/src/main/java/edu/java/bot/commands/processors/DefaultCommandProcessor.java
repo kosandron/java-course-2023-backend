dde52c327d1b8fe22e9d8a98a4550744697dfc4c
Andrei Kostygov <kosandr17@mail.ru>
2024-04-14 17:02:34 +0300

    hw9

--- bot/src/main/java/edu/java/bot/commands/processors/DefaultCommandProcessor.java
+++ bot/src/main/java/edu/java/bot/commands/processors/DefaultCommandProcessor.java
@@ -3,15 +3,21 @@ package edu.java.bot.commands.processors;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.commands.Command;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component("DefaultCommandProcessor")
-@RequiredArgsConstructor
 public class DefaultCommandProcessor implements CommandProcessor {
     private static final String UNSUPPORTED_COMMAND = "Unsopported command!";
     private final List<Command> commands;
+    private final Counter counter;
+
+    public DefaultCommandProcessor(List<Command> commands, MeterRegistry meterRegistry) {
+        this.commands = commands;
+        this.counter = meterRegistry.counter("message_processed_count");
+    }
 
     @Override
     public List<Command> commands() {
@@ -20,6 +26,7 @@ public class DefaultCommandProcessor implements CommandProcessor {
 
     @Override
     public SendMessage process(Update update) {
+        counter.increment();
         for (var command : commands) {
             if (command.supports(update)) {
                 return command.handle(update);
